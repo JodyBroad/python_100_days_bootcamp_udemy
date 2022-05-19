b6f7a6ca56a4d338cc11e000a16b440a7e65fc9d
Jody Broad <msjodybroad@gmail.com>
2022-05-19 16:10:51 +0100

    day one exercises not in replit

--- day_one.py
+++ day_one.py
@@ -1,1 +1,8 @@
-#start
+# basic print
+print("Hello world!")
+# print with a new line \n
+print("Hello world!\nHello world!\nHello World!")
+# concatenation
+print("Hello" + " Jody")
+# takes user input and concatenates - the input statement will happen first
+print("Hello " + input("What is your name?") + "!")
